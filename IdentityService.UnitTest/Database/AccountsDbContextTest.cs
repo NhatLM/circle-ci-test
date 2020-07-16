41a53f14b522e744fd382c6bfd3c2c0696376cd4
Le Minh Nhat <lmn@netcompany.com>
2020-07-16 16:39:22 +0700

    init fake dbContext and helper

---
+++ IdentityService.UnitTest/Database/AccountsDbContextTest.cs
@@ -0,0 +1,15 @@
+﻿using IdentityService.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.UnitTest.Database
+{
+    class AccountsDbContextTest : AccountsContext
+    {
+        public AccountsDbContextTest() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "AccountDbTest");
+        }
+    }
+}
